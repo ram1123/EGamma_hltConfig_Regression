076c54a2de1550ef6f3bf5660ff8799be66d4c90
Ram Krishna Sharma <ram.krishna.sharma@cern.ch>
2022-03-09 13:27:31 +0100

    Updated hlt_real_CMSSWComp.py

--- hlt_real_CMSSWComp.py
+++ hlt_real_CMSSWComp.py
@@ -3296,44 +3296,44 @@ process.dqmOutput = cms.OutputModule("DQMRootOutputModule",
 
 process.DQMOutput = cms.EndPath( process.dqmOutput )
 
-process.scEnergyCorr = cms.EDProducer( "SCEnergyCorrectorProducer",
-    correctorCfg = cms.PSet(
-    isHLT = cms.bool(True),
-    isPhaseII = cms.bool(False),
-    applySigmaIetaIphiBug = cms.bool(True),
-    ),
-    inputSCs = cms.InputTag('particleFlowSuperClusterECAL'),
-    writeFeatures = cms.bool(True)
-)
-
-#  process.scEnergyCorr = cms.EDProducer('SCEnergyCorrectorProducer',
-#   correctorCfg = cms.PSet(
+# process.scEnergyCorr = cms.EDProducer( "SCEnergyCorrectorProducer",
+#     correctorCfg = cms.PSet(
 #     isHLT = cms.bool(True),
 #     isPhaseII = cms.bool(False),
-#     applySigmaIetaIphiBug = cms.bool(False),
-#     ecalRecHitsEE = cms.InputTag('ecalRecHit', 'EcalRecHitsEE'),
-#     ecalRecHitsEB = cms.InputTag('ecalRecHit', 'EcalRecHitsEB'),
-#     regressionKeyEB = cms.string('pfscecal_EBCorrection_offline_v2'),
-#     regressionKeyEE = cms.string('pfscecal_EECorrection_offline_v2'),
-#     uncertaintyKeyEB = cms.string('pfscecal_EBUncertainty_offline_v2'),
-#     uncertaintyKeyEE = cms.string('pfscecal_EEUncertainty_offline_v2'),
-#     regressionMinEB = cms.double(0.2),
-#     regressionMaxEB = cms.double(2),
-#     regressionMinEE = cms.double(0.2),
-#     regressionMaxEE = cms.double(2),
-#     uncertaintyMinEB = cms.double(0.0002),
-#     uncertaintyMaxEB = cms.double(0.5),
-#     uncertaintyMinEE = cms.double(0.0002),
-#     uncertaintyMaxEE = cms.double(0.5),
-#     vertexCollection = cms.InputTag('offlinePrimaryVertices'),
-#     eRecHitThreshold = cms.double(1),
-#     hgcalRecHits = cms.InputTag(''),
-#     hgcalCylinderR = cms.double(2.7999999523162842)
-#   ),
-#   writeFeatures = cms.bool(True),
-#   inputSCs = cms.InputTag('particleFlowSuperClusterECAL'),
-#   mightGet = cms.optional.untracked.vstring
+#     applySigmaIetaIphiBug = cms.bool(True),
+#     ),
+#     inputSCs = cms.InputTag('particleFlowSuperClusterECAL'),
+#     writeFeatures = cms.bool(True)
 # )
+
+process.scEnergyCorr = cms.EDProducer('SCEnergyCorrectorProducer',
+  correctorCfg = cms.PSet(
+    isHLT = cms.bool(True),
+    isPhaseII = cms.bool(False),
+    applySigmaIetaIphiBug = cms.bool(False),
+    ecalRecHitsEE = cms.InputTag('ecalRecHit', 'EcalRecHitsEE'),
+    ecalRecHitsEB = cms.InputTag('ecalRecHit', 'EcalRecHitsEB'),
+    regressionKeyEB = cms.string('pfscecal_EBCorrection_offline_v2'),
+    regressionKeyEE = cms.string('pfscecal_EECorrection_offline_v2'),
+    uncertaintyKeyEB = cms.string('pfscecal_EBUncertainty_offline_v2'),
+    uncertaintyKeyEE = cms.string('pfscecal_EEUncertainty_offline_v2'),
+    regressionMinEB = cms.double(0.2),
+    regressionMaxEB = cms.double(2),
+    regressionMinEE = cms.double(0.2),
+    regressionMaxEE = cms.double(2),
+    uncertaintyMinEB = cms.double(0.0002),
+    uncertaintyMaxEB = cms.double(0.5),
+    uncertaintyMinEE = cms.double(0.0002),
+    uncertaintyMaxEE = cms.double(0.5),
+    vertexCollection = cms.InputTag('offlinePrimaryVertices'),
+    eRecHitThreshold = cms.double(1),
+    hgcalRecHits = cms.InputTag(''),
+    hgcalCylinderR = cms.double(2.7999999523162842)
+  ),
+  writeFeatures = cms.bool(True),
+  inputSCs = cms.InputTag('particleFlowSuperClusterECAL'),
+  mightGet = cms.optional.untracked.vstring
+)
 # add specific customizations
 _customInfo = {}
 _customInfo['menuType'  ]= "GRun"
